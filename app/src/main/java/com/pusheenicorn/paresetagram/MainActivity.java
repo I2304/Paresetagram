610bf733006cfaa920de20411274a7086ac4d503
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-11 11:19:30 -0700

    Can save image now.

--- app/src/main/java/com/pusheenicorn/paresetagram/MainActivity.java
+++ app/src/main/java/com/pusheenicorn/paresetagram/MainActivity.java
@@ -1,52 +1,29 @@
 package com.pusheenicorn.paresetagram;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.pusheenicorn.paresetagram.Models.Post;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_TAKE_PHOTO = 1;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    ImageView ivImage;
-    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkPermissionsPlease();
-        ivImage = (ImageView) findViewById(R.id.ivImage);
     }
 
-    private void checkPermissionsPlease() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
-        }
+    public void onPost(View v) {
+        Intent intent = new Intent(MainActivity.this, PostActivity.class);
+        startActivity(intent);
     }
 
     private void loadTopPosts() {
@@ -75,55 +52,5 @@ public class MainActivity extends AppCompatActivity {
         ParseUser.logOut();
         Intent i = new Intent(MainActivity.this, HomeActivity.class);
         startActivity(i);
-    }
-
-    public void dispatchTakePictureIntent(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.pusheenicorn.paresetagram",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            Bitmap bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-            ivImage.setImageBitmap(bitmap);
-        }
-    }
-
-    String mCurrentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
     }
 }
---
+++ app/src/main/java/com/pusheenicorn/paresetagram/PostActivity.java
@@ -0,0 +1,142 @@
+package com.pusheenicorn.paresetagram;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.pusheenicorn.paresetagram.Models.Post;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PostActivity extends AppCompatActivity {
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public Bitmap bitmap;
+
+    ImageView ivImage;
+    public File photoFile;
+    EditText etDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        checkPermissionsPlease();
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+        etDescription = (EditText) findViewById(R.id.etDescription);
+    }
+
+    private void checkPermissionsPlease() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+
+    public void onLogout(View view) {
+        ParseUser.logOut();
+        Intent i = new Intent(PostActivity.this, HomeActivity.class);
+        startActivity(i);
+    }
+
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.pusheenicorn.paresetagram",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            bitmap = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            ivImage.setImageBitmap(bitmap);
+        }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void onSubmit(View v) {
+        ParseFile parseFile = new ParseFile(photoFile);
+        String description = etDescription.getText().toString();
+        ParseUser user = ParseUser.getCurrentUser();
+        createPost(description, parseFile, user);
+    }
+
+    private void createPost(final String description, final ParseFile imageFile,
+                            final ParseUser user) {
+        imageFile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    final Post newPost = new Post();
+                    newPost.setDescription(description);
+                    newPost.setImage(imageFile);
+                    newPost.setUser(user);
+                    newPost.saveInBackground();
+                    etDescription.setText("");
+
+                    Intent intent = new Intent(PostActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
