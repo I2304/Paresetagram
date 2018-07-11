3485e04e9f43c1ae68a6d8aab4698af0c4285bd1
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-11 14:57:09 -0700

    Added timeline!

--- app/src/main/java/com/pusheenicorn/paresetagram/MainActivity.java
+++ app/src/main/java/com/pusheenicorn/paresetagram/MainActivity.java
@@ -1,26 +1,68 @@
 package com.pusheenicorn.paresetagram;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.pusheenicorn.paresetagram.Models.Post;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    PostAdapter postAdapter;
+    ArrayList<Post> posts;
+    RecyclerView rvPost;
+
+    ProgressBar progressBar;
+    private SwipeRefreshLayout swipeContainer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+        progressBar = (ProgressBar) findViewById(R.id.pbLoading);
+        rvPost = (RecyclerView) findViewById(R.id.rvPost);
+        posts = new ArrayList<Post>();
+        // construct the adapter from this data source
+        postAdapter = new PostAdapter(posts);
+        // recycler view setup
+        rvPost.setLayoutManager(new LinearLayoutManager(this));
+        rvPost.setAdapter(postAdapter);
 
         loadTopPosts();
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     public void onPost(View v) {
@@ -38,16 +80,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void done(List<Post> objects, ParseException e) {
                 if (e == null) {
-                    for (int i = 0; i < objects.size(); i++) {
-                        Log.d("MainActivity", "Post[" + i + "] = "
-                                + objects.get(i).getDescription()
-                                + "\nusername = " + objects.get(i).getUser().getUsername());
-                    }
+                    // add the objects
+                    posts.addAll(objects);
+                    // notify the adapter
+                    postAdapter.notifyDataSetChanged();
                 } else {
                     e.printStackTrace();
                 }
             }
         });
+    }
+
+    public void fetchTimelineAsync(int page) {
+        // TODO - do stuff
     }
 
     public void onLogout(View view) {
---
+++ app/src/main/java/com/pusheenicorn/paresetagram/PostAdapter.java
@@ -0,0 +1,92 @@
+package com.pusheenicorn.paresetagram;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.pusheenicorn.paresetagram.Models.Post;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder>{
+    List<Post> mPosts;
+    Context context;
+
+    // constructor
+    public PostAdapter(List<Post> posts) {
+        mPosts = posts;
+    }
+
+    @NonNull
+    @Override
+    public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View postView = inflater.inflate(R.layout.item_post, parent, false);
+        ViewHolder viewHolder = new ViewHolder(postView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        // get the data according to position
+        Post post = mPosts.get(position);
+
+        // populate the views according to this data
+        holder.tvDescription.setText(post.getDescription());
+        holder.tvUser.setText(post.getUser().getUsername());
+
+        Glide.with(context).load(post.getImage().getUrl()).into(holder.ivImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        mPosts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        mPosts.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    // create ViewHolder class
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @Nullable
+        @BindView(R.id.ivImage)
+        ImageView ivImage;
+
+        @BindView(R.id.tvDescription) TextView tvDescription;
+        @BindView(R.id.tvUser) TextView tvUser;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        // when the user clicks on a row, show DetailsActivity for the selected movie
+        public void onClick(View v) {
+            // TODO - add a detail view
+        }
+    }
+
+}
