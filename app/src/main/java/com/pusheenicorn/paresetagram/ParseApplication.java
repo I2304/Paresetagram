ea1688ef6046fa5fd5c2d0cfeb955acdccb74a10
Ishani Karmarkar <ikarmarkar@fb.com>
2018-07-10 17:12:21 -0700

    Added camera functionality

--- app/src/main/java/com/pusheenicorn/paresetagram/ParseApplication.java
+++ app/src/main/java/com/pusheenicorn/paresetagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.pusheenicorn.paresetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
+import com.pusheenicorn.paresetagram.Models.Post;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         final Parse.Configuration configuration = new Parse.Configuration.Builder(this)
                 .applicationId("ishani-karmarkar")
